6419ec192a5d7869feb1bcd84bede635b054d0df
SlavaZinevichUSC <97628779+SlavaZinevichUSC@users.noreply.github.com>
2022-04-19 16:00:34 -0700

    Initial Commit

---
+++ slava-stock-watch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slava-stock-watch
+//
+//  Created by Slava Zinevich on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slava-stock-watch/slava_stock_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  slava_stock_watchApp.swift
+//  slava-stock-watch
+//
+//  Created by Slava Zinevich on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct slava_stock_watchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
